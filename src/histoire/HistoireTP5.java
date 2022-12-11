35694009fa90889c4d2acdf49ead1685c1f3ba6b
william-valenduc <william.valenduc@orange.fr>
2022-12-11 16:19:28 +0100

    les samourais

--- src/histoire/HistoireTP5.java
+++ src/histoire/HistoireTP5.java
@@ -3,23 +3,30 @@ package histoire;
 import personnages.Commercant;
 import personnages.Humain;
 import personnages.Ronin;
+import personnages.Samourai;
 import personnages.Yakuza;
 
 public class HistoireTP5 {
 	public static void main(String[] args) {
 		
 		Commercant marco = new Commercant("Marco", 20);
-		Commercant chonin = new Commercant("Chonin", 40);
-		Commercant kumi = new Commercant("Kumi", 10);
-		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
-		Ronin roro = new Ronin("Roro", "shochu", 60);
+//		Commercant chonin = new Commercant("Chonin", 40);
+//		Commercant kumi = new Commercant("Kumi", 10);
+//		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+//		Ronin roro = new Ronin("Roro", "shochu", 60);
+//		
+//		marco.faireConnaissance(roro);
+//		marco.faireConnaissance(yaku);
+//		marco.faireConnaissance(chonin);
+//		marco.faireConnaissance(kumi);
+//		marco.listerConnaissance();
+//		roro.listerConnaissance();
+//		yaku.listerConnaissance();
 		
-		marco.faireConnaissance(roro);
-		marco.faireConnaissance(yaku);
-		marco.faireConnaissance(chonin);
-		marco.faireConnaissance(kumi);
-		marco.listerConnaissance();
-		roro.listerConnaissance();
-		yaku.listerConnaissance();
+		Samourai akimoto = new Samourai("Miyamoto", "Akimoto", "saké", 80);
+		akimoto.faireConnaissance(marco);
+		akimoto.listerConnaissance();
+		marco.boire();
+		akimoto.boire("thé");
 	}
 }
